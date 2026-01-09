e78fea9f703f9940a210e82cbbe43d2a99f012d6
varunigaur07-lab <varunigaur07@gmail.com>
2026-01-09 12:17:56 +0530

    Update Main.java
    a+b

--- src/Main.java
+++ src/Main.java
@@ -5,10 +5,12 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
 
         // TODO: Read two integers
-      
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int rslt = a+b;
 
         // TODO: Print their sum  using println
-      
+        System.out.println(rslt);
         scanner.close();
     }
 }
